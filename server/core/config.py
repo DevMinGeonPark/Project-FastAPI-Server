387dd2ee3161c660fc5a92214f2b20830c0970bf
DevMinGeonPark <devmin7985@gmail.com>
2022-05-31 15:11:19 +0900

    remove comment

--- server/core/config.py
+++ server/core/config.py
@@ -13,19 +13,6 @@ class Settings:
     DB_PORT : str = os.getenv("DB_PORT")
     DB_DATABASE : str = os.getenv("DB_DATABASE")
 	
-    DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}?charset=utf8"
+    DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
 
 settings = Settings()
-
-
-# user_name = "min"
-# user_pwd = "fksak12@@K"
-# db_host = "130.162.159.163"
-# db_name = "testdb"
-
-# DATABASE = 'mysql+pymysql://%s:%s@%s/%s?charset=utf8' % (
-#     user_name,
-#     user_pwd,
-#     db_host,
-#     db_name,
-# )
